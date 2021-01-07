9a8bebb3122a6aeb766ee513c27903c4dc1ea71c
Parameswari <priyanka10params@gmail.com>
2021-01-07 15:27:56 +0530

    last commit

--- src/test/java/com/grootan/qa/testcases/GrootanTest.java
+++ src/test/java/com/grootan/qa/testcases/GrootanTest.java
@@ -1,9 +1,7 @@
 package com.grootan.qa.testcases;
 
 
-import org.automationtesting.excelreport.Xl;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
